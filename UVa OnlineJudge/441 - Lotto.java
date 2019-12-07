740d44344a07fea3bac2a0424bcba20599c686f3
Kavin K G <41513952+kavingovind@users.noreply.github.com>
2019-12-07 12:39:40 +0530

    Add files via upload

---
+++ UVa OnlineJudge/441 - Lotto.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+	  public static void main (String[] args)
+	  {
+		    Scanner ip=new Scanner(System.in);
+        int arrsize=ip.nextInt();
+        int count=0;
+        while(true)
+        {
+          if(arrsize==0)
+          {
+            return;
+          }
+          else if(count!=0)
+          {
+            System.out.println();
+          }
+          count++;
+	        int arr[]=new int[arrsize];
+	        for(int i=0;i<arrsize;i++)
+	        {
+    	        arr[i]=ip.nextInt();
+	        }
+	        for(int i=0;i<arrsize;i++)
+	        {
+                for(int j=i+1;j<arrsize;j++)
+                {
+                    for(int k=j+1;k<arrsize;k++)
+                    {
+                        for(int l=k+1;l<arrsize;l++)
+                        {
+                            for(int m=l+1;m<arrsize;m++)
+                            {
+                                for(int n=m+1;n<arrsize;n++)
+                                {
+                                    System.out.println(arr[i]+" "+arr[j]+" "+arr[k]+" "+arr[l]+" "+arr[m]+" "+arr[n]);   
+                                }
+                            }
+                        }
+                    }
+                }
+	        }
+	    }
+	}
+}
